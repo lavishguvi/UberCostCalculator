315f1ae7df3344fca9dbeddf1d85d71788fb5b20
lavish <lavish@guvi.in>
2023-01-31 18:01:16 +0530

    Uber cost Calculator

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const title = document.querySelector('#title');
-const weekday = document.querySelector('#weekday');
-const date = new Date();
-weekday.innerHTML = weekdays[date.getDay()];
+const form = document.querySelector("form");
+      const output = document.querySelector(".output");
+
+      form.addEventListener("submit", event => {
+        event.preventDefault();
+
+        const distance = parseFloat(document.querySelector("#distance").value);
+        const baseFare = parseFloat(document.querySelector("#baseFare").value);
+        const costPerMinute = parseFloat(document.querySelector("#costPerMinute").value);
+        const costPerMile = parseFloat(document.querySelector("#costPerMile").value);
+
+        const totalCost = baseFare + costPerMinute * distance + costPerMile * distance;
+
+        output.innerHTML = `The total cost of the Uber ride is Rs.${totalCost.toFixed(2)}.`;
+      });
