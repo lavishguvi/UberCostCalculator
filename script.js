6961a9079001ddfe6066699334395abfcd66db3c
lavish <lavish@guvi.in>
2023-01-31 16:32:24 +0530

    added color picker project using dom

---
+++ script.js
@@ -0,0 +1,19 @@
+let colors = [ "red","blue", "orange", "green", "yellow", "indigo", "violet"];
+
+let container = document.querySelector('#container');
+let h1 = document.querySelector('h1');
+let clickedColor = document.querySelector('#clickedColor');
+
+for (const color of colors) {
+  let box = document.createElement('div');
+  box.classList.add('box');
+  container.appendChild(box);
+
+  box.style.backgroundColor = color;
+  box.addEventListener("click", function(){
+      h1.innerHTML = "You have selected: ";
+      clickedColor.innerHTML = color;
+      clickedColor.style.backgroundColor = color;
+  });
+
+};
