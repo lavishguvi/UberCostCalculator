61a37d2c92b081d7d7377b3f9ef376592c3803b0
lavish <lavish@guvi.in>
2023-01-31 17:10:00 +0530

    display a local time

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-let colors = [ "red","blue", "orange", "green", "yellow", "indigo", "violet"];
-
-let container = document.querySelector('#container');
-let h1 = document.querySelector('h1');
-let clickedColor = document.querySelector('#clickedColor');
-
-for (const color of colors) {
-  let box = document.createElement('div');
-  box.classList.add('box');
-  container.appendChild(box);
-
-  box.style.backgroundColor = color;
-  box.addEventListener("click", function(){
-      h1.innerHTML = "You have selected: ";
-      clickedColor.innerHTML = color;
-      clickedColor.style.backgroundColor = color;
-  });
-
-};
+function updateTime() {
+  let date = new Date();
+  let time = date.toLocaleTimeString();
+  document.getElementById("time").innerHTML = time;
+}
+setInterval(updateTime, 1000);
